3abf47ecfb6998c0be14b3e832450518882c63ae
Christopher Jones <thekirkconcept@gmail.com>
2018-11-20 09:31:31 -0800

    Update test.vue

--- src/components/test.vue
+++ src/components/test.vue
@@ -8,7 +8,7 @@
         <input type="text" placeholder="Mad" v-model="thoughts">
         <p><b>What I am thinking:</b> {{ thoughts }}</p>
         <ul class="hobbies">
-            <li v-for="item in items">{{item.title}}</li>
+            <li v-bind:key="item" v-for="item in items">{{item.title}}</li>
         </ul>
         <button v-on:click="greet('Hi, Bud')">Click Me</button>
 <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
@@ -60,7 +60,7 @@
         padding: 10px 1%;
         background: orange;
         display: inline-block;
-        float: left;
+
         margin: 0;
     }
  </style>
